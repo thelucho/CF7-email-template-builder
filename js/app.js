4b91b81b8f5cb192ce1caa9943efdb5cdcb845d5
Luciano Dichiara <luciano@studiohakuna.com>
2020-04-24 01:58:00 -0300

    Se agregó ColorPicker + cambios en Code Snippet (css + condicionales)

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 ----------
 TO DO:
 ----------
-- Agregar .gitignore (sass-cache, /sass)
+- Agregar resto de condicionales en Template
 - Agregar 'Add Field' en Campos + Array dinamico
 - Agregar field 'Color' para Header y Footer
 - Formatear Caja de Code Snippet
@@ -20,7 +20,7 @@ const preview = document.getElementById('preview');
 const code = document.getElementById('code-box');
 
 
-
+addEventListener('DOMContentLoaded', generarTemplate);
 // Event Listeners
 formulario.addEventListener('submit', generarTemplate);
 
@@ -28,51 +28,53 @@ formulario.addEventListener('submit', generarTemplate);
 
 // Funciones
 function generarTemplate(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const infoTemplate = {
-        logoHeaderLink: formulario.querySelector('#logo-url-header').value,
-        logoHeaderImg: formulario.querySelector('#logo-img-header').value,
+  const infoTemplate = {
+    logoHeaderLink: formulario.querySelector('#logo-url-header').value,
+    logoHeaderImg: formulario.querySelector('#logo-img-header').value,
+    bgColorHeader: formulario.querySelector('#bg-color-header').value,
 
-        asunto: formulario.querySelector('#asunto').value,
+    asunto: formulario.querySelector('#asunto').value,
 
-        campo_nombre_1: formulario.querySelector('#campo1-nombre').value,
-        campo_shortcode_1: formulario.querySelector('#campo1-shortcode').value,
-        campo_nombre_2: formulario.querySelector('#campo2-nombre').value,
-        campo_shortcode_2: formulario.querySelector('#campo2-shortcode').value,
-        campo_nombre_3: formulario.querySelector('#campo3-nombre').value,
-        campo_shortcode_3: formulario.querySelector('#campo3-shortcode').value,
-        campo_nombre_4: formulario.querySelector('#campo4-nombre').value,
-        campo_shortcode_4: formulario.querySelector('#campo4-shortcode').value,
-        campo_nombre_5: formulario.querySelector('#campo5-nombre').value,
-        campo_shortcode_5: formulario.querySelector('#campo5-shortcode').value,
+    campo_nombre_1: formulario.querySelector('#campo1-nombre').value,
+    campo_shortcode_1: formulario.querySelector('#campo1-shortcode').value,
+    campo_nombre_2: formulario.querySelector('#campo2-nombre').value,
+    campo_shortcode_2: formulario.querySelector('#campo2-shortcode').value,
+    campo_nombre_3: formulario.querySelector('#campo3-nombre').value,
+    campo_shortcode_3: formulario.querySelector('#campo3-shortcode').value,
+    campo_nombre_4: formulario.querySelector('#campo4-nombre').value,
+    campo_shortcode_4: formulario.querySelector('#campo4-shortcode').value,
+    campo_nombre_5: formulario.querySelector('#campo5-nombre').value,
+    campo_shortcode_5: formulario.querySelector('#campo5-shortcode').value,
 
-        logoFooterLink: formulario.querySelector('#logo-url-footer').value,
-        logoFooterImg: formulario.querySelector('#logo-img-footer').value
-    }
+    logoFooterLink: formulario.querySelector('#logo-url-footer').value,
+    logoFooterImg: formulario.querySelector('#logo-img-footer').value,
+    bgColorFooter: formulario.querySelector('#bg-color-footer').value
+  }
 
-    imprimirCodigo(infoTemplate);
+  imprimirCodigo(infoTemplate);
 }
+
 
 
 function imprimirCodigo(template) {
 
-    const div = document.createElement('div');
+  const div = document.createElement('div');
 
-    div.innerHTML =
-        `
+  div.innerHTML =
+    `
             <table bgcolor="#f0f0f0" width="100%" border="0" cellspacing="0" cellpadding="0" align="center"
                 style="padding-top: 70px; padding-bottom: 70px;">
                 <tbody>
                   <tr>
                     <td>
-                      <table align="center" bgcolor="#fff" border="0" width="600" style="background-color: #fff;">
+                      <table align="center" bgcolor="${template.bgColorHeader}" border="0" width="600">
                         <tbody>
                           <tr>
                             <td align="center" valign="bottom" style="padding-top: 10px;">
-                              <a href="${template.logoHeaderLink}" style="border:none;" target="_blank">
-                              <img src="${template.logoHeaderImg}"
-                                  alt="Strive Training Logo" style="border:none; width: 40%; margin-top: 25px;"></a>
+                              ${template.logoHeaderLink ? `<a href="${template.logoHeaderLink}" style="border:none;" target="_blank">` : `<a href="#" style="border:none;">`}
+                              <img src="${template.logoHeaderImg}" style="border:none; max-width: 190px; margin: 25px 0px;"></a>
                             </td>
                           </tr>
                           <tr>
@@ -98,9 +100,12 @@ function imprimirCodigo(template) {
                                 </tr>
                                 <tr>
                                   <td width="600" valign="center"
-                                    style="font-family:Arial; font-size:14px; text-align: center; color:#ffffff; padding: 35px 0px; background-color:#98498D; ">
-                                    <p><b><a href="${template.logoFooterLink}" target="_blank"
-                                          style="color:#ffffff;"><img src="${template.logoFooterImg}" /></a></b></p>
+                                    style="font-family:Arial; font-size:14px; text-align: center; color:#ffffff; padding: 35px 0px; background-color:${template.bgColorFooter}; ">
+                                    <p>
+                                      <a href="${template.logoFooterLink}" target="_blank" style="color:#ffffff;">
+                                          <img src="${template.logoFooterImg}" style="border:none; max-width: 190px; margin: 25px 0px;" />
+                                      </a>
+                                    </p>
                                   </td>
                                 </tr>
                               </tbody>
@@ -116,12 +121,20 @@ function imprimirCodigo(template) {
     `;
 
 
-    // Inserta el div con el template armado en '#preview'
+  // Inserta el div con el template armado en '#preview'
+  if (preview.hasChildNodes()) {
+    preview.removeChild(preview.childNodes[0]);
     preview.appendChild(div);
+  } else {
+    preview.appendChild(div);
+  }
 
-    // Inserta el codigo en el div '#code'
-    code.innerHTML = div.innerHTML;
+
+  // Inserta el codigo en el div '#code'
+  code.innerHTML = div.innerHTML;
 
 }
+
+
 
 
